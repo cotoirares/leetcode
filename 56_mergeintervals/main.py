fca9ad61f4cca36138a71df2d7ef9b7e843e0c81
Rares-Andrei Cotoi <62752604+coty1@users.noreply.github.com>
2024-11-07 13:33:35 +0200

    firs push

---
+++ 56_mergeintervals/main.py
@@ -0,0 +1,13 @@
+def findXSum(self, nums, k, x):
+    """
+    :type nums: List[int]
+    :type k: int
+    :type x: int
+    :rtype: List[int]
+    """
+    hash_map = [0 for i in range(0, 51)]
+    print(hash_map)
+
+
+if __name__ == "__main__":
+    findXSum([1, 2, 3, 4, 5], 3, 5)
